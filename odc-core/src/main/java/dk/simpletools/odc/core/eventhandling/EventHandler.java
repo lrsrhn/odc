09f6ba9f41e443b44b2520027c7718d88e2dd3f7
Lars Storm <lars.storm@systematic.com>
2019-03-27 08:20:01 +0100

    [(master)] Move packages, folders and maven modules to ott

--- odc-core/src/main/java/dk/simpletools/odc/core/eventhandling/EventHandler.java
+++
@@ -1,9 +0,0 @@
-package dk.simpletools.odc.core.eventhandling;
-
-import dk.simpletools.odc.core.finder.ElementHandler;
-import dk.simpletools.odc.core.processing.ObjectStore;
-import dk.simpletools.odc.core.processing.StructureElement;
-
-public interface EventHandler extends ElementHandler {
-    void handle(StructureElement structureElement, ObjectStore objectStore);
-}
--- odc-core/src/main/java/dk/simpletools/odc/core/finder/OnTextHandler.java
+++
@@ -1,8 +0,0 @@
-package dk.simpletools.odc.core.finder;
-
-import dk.simpletools.odc.core.processing.ObjectStore;
-import dk.simpletools.odc.core.processing.StructureElement;
-
-public interface OnTextHandler {
-    void onText(StructureElement structureElement, ObjectStore objectStore) throws Exception;
-}
